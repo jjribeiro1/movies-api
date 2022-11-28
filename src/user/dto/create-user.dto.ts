ddbc876879d4fd8fe4bfc46d620dca8c3c1d608e
jjribeiro1 <jvsr.99@hotmail.com>
2022-11-28 17:47:37 -0300

    "validando campos name, email e password com class-validator"

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,7 +1,19 @@
+import { IsString, MinLength, IsEmail, IsNotEmpty } from 'class-validator';
+import { IsValidPassword } from '../decorators/validPassword.decorator';
+
 export class CreateUserDto {
+  @IsString()
+  @MinLength(3)
+  @IsNotEmpty()
   name: string;
+
+  @IsEmail()
   email: string;
+
+  @IsValidPassword()
   password: string;
+
   cpf: string;
+
   role: string;
 }
