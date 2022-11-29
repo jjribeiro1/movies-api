a0c470c7566bd4c09976f5820339f9fea669117b
jjribeiro1 <jvsr.99@hotmail.com>
2022-11-29 16:41:49 -0300

    iniciando as configurações do swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Watch Movies')
+    .setDescription(
+      'Api para gerenciamento e avaliação de filmes de diferentes serviços de streaming',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
