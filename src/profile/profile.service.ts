6db54fe01e2f5f277bbb99a3a389e7a3aaeb3c92
jjribeiro1 <jvsr.99@hotmail.com>
2022-12-04 18:45:22 -0300

    "profile service"

---
+++ src/profile/profile.service.ts
@@ -0,0 +1,42 @@
+import { Exception, ExceptionsType } from 'src/exceptions/newException';
+import { ProfileRepository } from 'src/repositories/profile.repository';
+import { UserRepository } from 'src/repositories/user.repository';
+import { CreateProfileDto } from './dto/create-profile.dto';
+import { UpdateProfileDto } from './dto/update-profile.dto';
+import { ProfileEntity } from './entities/profile.entity';
+
+export class ProfileService {
+  constructor(
+    private readonly profileRepository: ProfileRepository,
+    private readonly userRepository: UserRepository,
+  ) {}
+
+  async create(dto: CreateProfileDto): Promise<ProfileEntity> {
+    const data = { ...dto };
+    const profileUser = await this.userRepository.findById(data.userId);
+    if (!profileUser) {
+      throw new Exception(ExceptionsType.INVALIDDATA, 'User not found');
+    }
+    return await this.profileRepository.create(data);
+  }
+
+  async findAll(): Promise<ProfileEntity[]> {
+    return await this.profileRepository.findAll();
+  }
+
+  async findOne(id: string): Promise<ProfileEntity> {
+    return await this.profileRepository.findById(id);
+  }
+
+  async update(id: string, dto: UpdateProfileDto): Promise<ProfileEntity> {
+    const data = {
+      name: dto.name,
+      imageUrl: dto.imageUrl,
+    };
+    return await this.profileRepository.update(id, data);
+  }
+
+  async delete(id: string): Promise<void> {
+    return await this.profileRepository.delete(id);
+  }
+}
