8ae80bbb0c7d93a855289fb589f84e7e916248f9
jjribeiro1 <jvsr.99@hotmail.com>
2022-12-04 21:25:38 -0300

    documentação: adicionando no swagger tag perfil no controller

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('status')
     .addTag('user')
+    .addTag('profile')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -7,16 +7,21 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { handleException } from 'src/exceptions/handleException';
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { UpdateProfileDto } from './dto/update-profile.dto';
 import { ProfileEntity } from './entities/profile.entity';
 import { ProfileService } from './profile.service';
 
+@ApiTags('profile')
 @Controller('profile')
 export class ProfileController {
   constructor(private readonly profileService: ProfileService) {}
 
+  @ApiOperation({
+    summary: 'Criar um novo perfil',
+  })
   @Post()
   async create(@Body() dto: CreateProfileDto): Promise<ProfileEntity> {
     try {
@@ -26,6 +31,9 @@ export class ProfileController {
     }
   }
 
+  @ApiOperation({
+    summary: 'Listar todos os perfis',
+  })
   @Get()
   async findAll(): Promise<ProfileEntity[]> {
     try {
@@ -35,6 +43,9 @@ export class ProfileController {
     }
   }
 
+  @ApiOperation({
+    summary: 'Listar um perfil por ID',
+  })
   @Get(':id')
   async findOne(@Param('id') id: string): Promise<ProfileEntity> {
     try {
@@ -44,6 +55,9 @@ export class ProfileController {
     }
   }
 
+  @ApiOperation({
+    summary: 'Atualizar um perfil por ID',
+  })
   @Patch(':id')
   async update(
     @Param('id') id: string,
@@ -56,6 +70,9 @@ export class ProfileController {
     }
   }
 
+  @ApiOperation({
+    summary: 'Deletar um perfil por ID',
+  })
   @Delete(':id')
   async remove(@Param('id') id: string): Promise<void> {
     try {
