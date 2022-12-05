405b949d18c20016f3fb7085f0dfa4c225e93fd4
jjribeiro1 <jvsr.99@hotmail.com>
2022-12-04 21:33:17 -0300

    "documentação: descrevendo o create-profile dto no swagger"

--- src/profile/dto/create-profile.dto.ts
+++ src/profile/dto/create-profile.dto.ts
@@ -1,5 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateProfileDto {
+  @ApiProperty({
+    description: 'Nome do perfil',
+    example: 'perfil 1',
+  })
   name: string;
+
+  @ApiProperty({
+    description: 'Url da imagem do perfil',
+    example: 'imagem.png',
+  })
   imageUrl: string;
+
+  @ApiProperty({
+    description: 'ID do usuário que criou o perfil',
+    example: 'd5b7ade1-679c-4e38-a6da-4298f49aef62',
+  })
   userId: string;
 }
