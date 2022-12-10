943c2c542cf2f537f168b65adf2b11fc787f855f
jjribeiro1 <jvsr.99@hotmail.com>
2022-12-10 19:44:24 -0300

    "validando name e imageUrl como propriedades unicas no create-movie dto"

--- src/movie/movie.module.ts
+++ src/movie/movie.module.ts
@@ -3,10 +3,11 @@ import { MovieService } from './movie.service';
 import { MovieController } from './movie.controller';
 import { MovieRepository } from 'src/repositories/movie.repository';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { IsUniqueConstraint } from 'src/user/decorators/IsUnique.decorator';
 
 @Module({
   imports: [PrismaModule],
   controllers: [MovieController],
-  providers: [MovieService, MovieRepository],
+  providers: [MovieService, MovieRepository, IsUniqueConstraint],
 })
 export class MovieModule {}
