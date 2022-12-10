e7060d5e7e0bf5c18a5b1f7b355a0f6ea741629b
jjribeiro1 <jvsr.99@hotmail.com>
2022-12-09 21:52:50 -0300

    nest g resource movie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { UserModule } from '../user/user.module';
 import { ProfileModule } from 'src/profile/profile.module';
+import { MovieModule } from 'src/movie/movie.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, ProfileModule],
+  imports: [PrismaModule, UserModule, ProfileModule, MovieModule],
   controllers: [AppController],
   providers: [AppService],
 })
