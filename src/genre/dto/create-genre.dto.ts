57407d5acfdb9e23f75844f5251d0de4daea06b2
jjribeiro1 <jvsr.99@hotmail.com>
2022-12-10 20:35:24 -0300

    "validação no create-movie dto"

--- src/genre/dto/create-genre.dto.ts
+++ src/genre/dto/create-genre.dto.ts
@@ -1,3 +1,9 @@
+import { IsString, MinLength } from 'class-validator';
+import { IsUnique } from 'src/user/decorators/IsUnique.decorator';
+
 export class CreateGenreDto {
+  @IsString()
+  @MinLength(3)
+  @IsUnique('genre')
   name: string;
 }
