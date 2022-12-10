b83d2aaba712816598003b03b0dc1d206a7e1bd9
jjribeiro1 <jvsr.99@hotmail.com>
2022-12-09 23:55:48 -0300

    "movie controller e modulo"

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -10,33 +10,58 @@ import {
 import { MovieService } from './movie.service';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
+import { handleException } from 'src/exceptions/handleException';
+import { MovieEntity } from './entities/movie.entity';
 
 @Controller('movie')
 export class MovieController {
   constructor(private readonly movieService: MovieService) {}
 
   @Post()
-  create(@Body() createMovieDto: CreateMovieDto) {
-    return this.movieService.create(createMovieDto);
+  async create(@Body() dto: CreateMovieDto): Promise<MovieEntity> {
+    try {
+      return await this.movieService.create(dto);
+    } catch (error) {
+      handleException(error);
+    }
   }
 
   @Get()
-  findAll() {
-    return this.movieService.findAll();
+  async findAll(): Promise<MovieEntity[]> {
+    try {
+      return await this.movieService.findAll();
+    } catch (error) {
+      handleException(error);
+    }
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.movieService.findOne(+id);
+  async findOne(@Param('id') id: string): Promise<MovieEntity> {
+    try {
+      return await this.movieService.findOne(id);
+    } catch (error) {
+      handleException(error);
+    }
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateMovieDto: UpdateMovieDto) {
-    return this.movieService.update(+id, updateMovieDto);
+  async update(
+    @Param('id') id: string,
+    @Body() dto: UpdateMovieDto,
+  ): Promise<MovieEntity> {
+    try {
+      return await this.movieService.update(id, dto);
+    } catch (error) {
+      handleException(error);
+    }
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.movieService.remove(+id);
+  async remove(@Param('id') id: string): Promise<void> {
+    try {
+      return await this.movieService.remove(id);
+    } catch (error) {
+      handleException(error);
+    }
   }
 }
--- src/movie/movie.module.ts
+++ src/movie/movie.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MovieService } from './movie.service';
 import { MovieController } from './movie.controller';
+import { MovieRepository } from 'src/repositories/movie.repository';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [MovieController],
-  providers: [MovieService],
+  providers: [MovieService, MovieRepository],
 })
 export class MovieModule {}
