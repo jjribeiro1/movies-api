43c0eeb383cf434289185c9e4a3050fd613ece8e
jjribeiro1 <jvsr.99@hotmail.com>
2022-12-10 22:01:59 -0300

    "adicionando validação no create-streaming dto"

--- src/streaming/dto/create-streaming.dto.ts
+++ src/streaming/dto/create-streaming.dto.ts
@@ -1,4 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
+import { IsUnique } from 'src/user/decorators/IsUnique.decorator';
+
 export class CreateStreamingDto {
+  @IsString()
+  @IsUnique('streaming')
+  @ApiProperty({
+    description: 'Nome do streaming',
+    example: 'HBO Plus',
+  })
   name: string;
+
+  @IsNumber()
+  @ApiProperty({
+    description: 'Preço do streaming',
+    example: 20,
+  })
   price: number;
 }
