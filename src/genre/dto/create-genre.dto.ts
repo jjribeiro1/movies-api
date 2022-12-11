1801187cd3d3067b5a78ba6457206c8925dababd
jjribeiro1 <jvsr.99@hotmail.com>
2022-12-10 22:17:00 -0300

    documentação: create-genre dto

--- src/genre/dto/create-genre.dto.ts
+++ src/genre/dto/create-genre.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, MinLength } from 'class-validator';
 import { IsUnique } from 'src/user/decorators/IsUnique.decorator';
 
@@ -5,5 +6,9 @@ export class CreateGenreDto {
   @IsString()
   @MinLength(3)
   @IsUnique('genre')
+  @ApiProperty({
+    description: 'nome do gênero',
+    example: 'ficção científica',
+  })
   name: string;
 }
