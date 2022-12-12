06f05f06f58349719f4f74209feb64a605ab22b5
jjribeiro1 <jvsr.99@hotmail.com>
2022-12-12 13:41:53 -0300

    "gerando o token jwt ao fazer login"

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,10 +4,14 @@ import { compare } from 'bcrypt';
 import { LoginOutPutDto } from './dto/login-output.dto';
 import { Exception, ExceptionsType } from 'src/exceptions/newException';
 import { UserRepository } from 'src/repositories/user.repository';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly jwt: JwtService,
+  ) {}
 
   async login(dto: LoginInputDto): Promise<LoginOutPutDto> {
     const { cpf, password } = dto;
@@ -33,7 +37,11 @@ export class AuthService {
 
     return {
       user,
-      token: 'token jwt',
+      token: this.jwt.sign({
+        sub: user.id,
+        email: user.email,
+        cpf: user.cpf,
+      }),
     };
   }
 }
