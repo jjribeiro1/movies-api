5dfe9ef706bfe0d6000bbc7d4fc85dac1e62cf0d
jjribeiro1 <jvsr.99@hotmail.com>
2022-12-12 17:13:57 -0300

    "jwt strategy e guard"

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { UserRepository } from 'src/repositories/user.repository';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly userRepository: UserRepository) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+  async validate(payload: { cpf: string }) {
+    console.log(payload);
+    const user = await this.userRepository.findByCpf(payload.cpf);
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    delete user.password;
+    return user;
+  }
+}
