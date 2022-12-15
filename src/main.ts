12145f17dd94b5fb92b90527eeeb5b5b6975ec46
jjribeiro1 <jvsr.99@hotmail.com>
2022-12-15 19:35:35 -0300

    "rejeita propriedades que não estão no dto"

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,12 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   app.enableCors();
   app.set('trust proxy', 1);
 
